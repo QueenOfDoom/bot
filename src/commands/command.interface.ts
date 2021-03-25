98680b7e04a3834d85c0c54ffe63f5e50429d251
David Kanter Eivin <kanter.dev@gmail.com>
2021-03-25 16:09:36 -0400

    feat: add permissions infrastructure

--- src/commands/command.interface.ts
+++ src/commands/command.interface.ts
@@ -1,10 +1,12 @@
 import { Message } from 'discord.js';
+import { PermissionsLevel } from '@/services/permissions.service';
 
 export type Trigger = string | RegExp | ((message: Message, command: string, args: string[]) => boolean);
 
 export interface ICommand {
     name: string;
     description: string;
+    requiredPerms: PermissionsLevel;
     triggers: Trigger[];
     validate: (message: Message, command: string, args: string[]) => boolean;
     execute: (message: Message, command: string, args: string[]) => void;
