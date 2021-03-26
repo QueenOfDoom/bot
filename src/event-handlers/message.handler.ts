c85293fe146414e717ac3ec29ba21f9cba025a86
Doomer <queenofdoom@protonmail.com>
2021-03-26 22:47:25 +0100

    fix: fixed prefix bug
    Fixed Prefix Bug, by extracting Prefix Part completely into
    the string-trigger condition. Thus args had to be added to
    ConcreteTrigger and are now per default the message - split
    by spaces. Changes have been made in Ping and Message Handler
    to use the changed architecture.

--- src/event-handlers/message.handler.ts
+++ src/event-handlers/message.handler.ts
@@ -1,6 +1,6 @@
 import { Message } from 'discord.js';
 import { IEventHandler } from './event-handler.interface';
-import {config} from '@/services/config.service';
+import { config } from '@/services/config.service';
 import { commands, defaultCommand } from '@/commands';
 import { RateLimitService } from '@/services/rate-limit.service';
 import { ConcreteTrigger, Command, TriggerCriteria } from '@/commands/command';
@@ -13,24 +13,19 @@ export class MessageHandler implements IEventHandler<MessageHandler['EVENT_NAME'
 
     public onEvent(message: Message): void {
         if (message.author.bot) return;
-        if (message.content.indexOf(config.botConfig.prefix)) return;
-
-        const args: string[] = message.content.slice(config.botConfig.prefix.length).split(/ +/g);
-        const keyword: string = args.shift()?.toLowerCase() || 'default';
-
+        
         // TODO: add check for moderation commands -- should bypass rate limit
         if (this.commandRateLimiter.shouldReject(message.author))
             return void message.channel.send('You are sending too many messages!');
 
+        const [command, trigger] = this.getTriggeredCommand(message) ?? [defaultCommand, NullTrigger];
 
-        const [command, trigger] = this.getTriggeredCommand(message, keyword, args) ?? [defaultCommand, NullTrigger];
-
-        command.execute(message, trigger, args);
+        command.execute(message, trigger, trigger.args);
     }
 
-    private getTriggeredCommand(message: Message, keyword: string, args: string[]): [Command, ConcreteTrigger] | undefined{
+    private getTriggeredCommand(message: Message): [Command, ConcreteTrigger] | undefined{
         for (const cmd of commands) {
-            const trigger = cmd.checkTriggers(message, keyword, args);
+            const trigger = cmd.checkTriggers(message);
             if (trigger)
                 return [cmd, trigger];
         }
