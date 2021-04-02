3ca2cc2e9268685ab8c2b5318e5f20d55b9e7bb9
Doomer <queenofdoom@protonmail.com>
2021-04-02 22:08:46 +0200

    feat: Expanded Help Test.

--- test/commands/help.test.ts
+++ test/commands/help.test.ts
@@ -7,6 +7,8 @@ import { ConcreteTrigger } from "@/commands/command";
 test('Help Command Trigger', () => {
     const msgA: Message = <Message> {content: `${config.botConfig.prefix}help`};
     const msgB: Message = <Message> {content: `${config.botConfig.prefix}helpsNotWorking`};
+    const msgC: Message = <Message> {content: `${config.botConfig.prefix}help --help`};
+    const msgD: Message = <Message> {content: `${config.botConfig.prefix}help --reload`};
     let trigger: ConcreteTrigger | null = null;
 
     // Alpha
@@ -23,6 +25,25 @@ test('Help Command Trigger', () => {
     commands.forEach(command => {
         trigger = command.checkTriggers(msgB, msgB.content.split(/ +/g)) || trigger;
     });
-
     expect(trigger).toBeNull();
+
+    // Gamma
+    trigger = null;
+    commands.forEach(command => {
+        trigger = command.checkTriggers(msgC, msgC.content.split(/ +/g)) || trigger;
+    });
+    expect(trigger).toBeDefined();
+    trig = <ConcreteTrigger> <unknown> trigger;
+    expect(trig.command.name).toBe('help');
+    expect(trig.activations[0].text).toBe('help>help');
+
+    // Delta
+    trigger = null;
+    commands.forEach(command => {
+        trigger = command.checkTriggers(msgD, msgD.content.split(/ +/g)) || trigger;
+    });
+    expect(trigger).toBeDefined();
+    trig = <ConcreteTrigger> <unknown> trigger;
+    expect(trig.command.name).toBe('reload');
+    expect(trig.activations[0].text).toBe('help>reload');
 })
